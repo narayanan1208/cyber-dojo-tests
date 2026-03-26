f9b0ec3ff5d32bfbcada7de1c57388f987d8a3f8
narayanan1208 <narayanan6647@gmail.com>
2026-03-26 16:53:09 +0530

    Added main file

---
+++ hiker.py
@@ -0,0 +1,15 @@
+def print_multiples_of_number_in_string():
+    first_digit, last_digit = 1, 100
+    for i in range(first_digit, last_digit + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+if __name__ == "__main__":
+    print_multiples_of_number_in_string()
